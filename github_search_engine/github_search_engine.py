ef12cdfaf02544c43f97d60121995069237e30eb
Sacha <sachahu@hotmail.fr>
2024-11-25 17:15:59 +0000

    feat: Add optional parameters to constructor and update search method
    - Added optional parameters to the constructor for flexibility.
    - Updated the search method to handle collection existence check and filtering empty issues.

--- github_search_engine/github_search_engine.py
+++ github_search_engine/github_search_engine.py
@@ -1,5 +1,8 @@
+import inspect
 import logging
+import sys
 from typing import List
+from typing import Optional
 
 import chevron
 from githubkit.versions.v2022_11_28.models import Issue
@@ -15,16 +18,20 @@ from github_search_engine.clients.ollama_client_manager import (
 
 
 class GithubSearchEngine:
-  def __init__(self, github_access_token: str):
+  def __init__(
+    self,
+    github_access_token: str,
+    qdrant_location: Optional[str] = None,
+    qdrant_path: Optional[str] = None,
+  ):
     logging.basicConfig(level=logging.WARNING)
 
     self._github_client = GithubClientManager(access_token=github_access_token)
-    self._database_client = QdrantClient(":memory:")
+    self._database_client = QdrantClient(
+      location=qdrant_location,
+      path=qdrant_path,
+    )
     self._ollama_client = OllamaClientManager()
-
-    self._collection = None
-    self._owner = None
-    self._repository_name = None
 
     self._database_client.set_model(
       "snowflake/snowflake-arctic-embed-m",
@@ -41,30 +48,17 @@ class GithubSearchEngine:
     return issue_summary
 
   def summarise_results(
-    self, results: List[QueryResponse], owner: str, repository_name: str
+    self,
+    results: List[QueryResponse],
+    owner: str,
+    repository_name: str,
+    query: str,
   ):
-    logging.info("Summarising issues")
-    issues = {
-      "issues": [
-        {
-          "issue": issue.metadata,
-          "comments": [
-            {"body": comment.body}
-            for comment in self._github_client.get_issue_comments(
-              owner=owner,
-              repository_name=repository_name,
-              issue_number=issue.metadata["number"],
-            )
-          ],
-        }
-        for issue in results
-      ]
-    }
     prompt_template = """
     Please briefly summarise the content and discussion of the following github issues.
-    Try to keep it short, concise and to the point.
+    Keep it short, concise and to the point and explain how it relates to '{{originalQuery}}'
+    Do not write headings or titles, simply summarize into a single 2-3 sentence paragraph.
 
-    {{#issues}}
       # {{issue.title}}
       {{issue.body}}
 
@@ -72,13 +66,37 @@ class GithubSearchEngine:
       {{#comments}}
       * {{body}}
       {{/comments}}
-    {{/issues}}
     """
-    summary = self._ollama_client.chat(
-      chevron.render(template=prompt_template, data=issues)
-    )
+    prompt_template = inspect.cleandoc(prompt_template)
+    logging.info("Summarising issues")
+    summaries = []
+    for issue in results:
+      summary = self._ollama_client.chat(
+        chevron.render(
+          template=prompt_template,
+          data={
+            "issue": issue.metadata,
+            "comments": [
+              {"body": comment.body}
+              for comment in self._github_client.get_issue_comments(
+                owner=owner,
+                repository_name=repository_name,
+                issue_number=issue.metadata["number"],
+              )
+            ],
+            "originalQuery": query,
+          },
+        )
+      )
+      summary = f"""
+      # Issue [#{issue.metadata["number"]}]({issue.metadata["html_url"]})
+      
+      {summary}
+      """
+      summaries.append(inspect.cleandoc(summary))
+    final_summary = "\n\n".join(summaries)
     logging.info("Done")
-    return summary
+    return final_summary
 
   async def index_repository(self, owner: str, repository_name: str):
     logging.info(f"Fetching Issues from {owner}/{repository_name}")
@@ -94,15 +112,24 @@ class GithubSearchEngine:
     )
     logging.info("Done")
 
-  def search(self, text: str) -> List[QueryResponse]:
-    results = self._database_client.query(
-      collection_name=self._collection,
-      query_text=text,
-      score_threshold=0.8,
-    )
-    return results
+  def search(
+    self, owner: str, repository_name: str, text: str
+  ) -> List[QueryResponse]:
+    if not self._database_client.collection_exists(
+      collection_name=f"{owner}/{repository_name}",
+    ):
+      logging.error(
+        "DB Collection not found. Try indexing the repository first."
+      )
+      sys.exit(1)
+    else:
+      results = self._database_client.query(
+        collection_name=f"{owner}/{repository_name}",
+        query_text=text,
+        score_threshold=0.8,
+        limit=5,
+      )
 
-  def set_collection(self, owner: str, repository_name: str):
-    self._collection = f"{owner}/{repository_name}"
-    self._owner = owner
-    self._repository_name = repository_name
+      # Filter empty issues
+      results = [result for result in results if result.metadata["body"]]
+      return results
