f552a3d1aacaf9e1f504501f266021f66d8834ae
semantic-release <semantic-release>
2024-11-25 18:33:37 +0000

    0.2.0
    Automatically generated by python-semantic-release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = "{% now 'utc', '%Y' %}, "
 author = "Sacha hu"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0-rc.1"
+release = "0.2.0"
 
 source_suffix = {
   ".rst": "restructuredtext",
